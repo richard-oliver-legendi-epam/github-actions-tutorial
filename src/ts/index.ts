82ee38d80e882de9e7cb9814acd07b9856bec4a1
richard-oliver-legendi-epam <richard_oliver_legendi@epam.com>
2023-03-27 17:48:49 +0200

    WHAT -- I cannot believe this is working lol

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,8 @@
 import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+//import * as bootstrap from 'bootstrap'
+
+import 'bootstrap'
 
 export * from './calculator'
