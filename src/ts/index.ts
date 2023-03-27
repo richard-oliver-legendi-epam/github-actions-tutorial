7f96360e664eaa3bea18423fcd0a1bd87ab72ae6
richard-oliver-legendi-epam <richard_oliver_legendi@epam.com>
2023-03-27 17:51:28 +0200

    Ok, fixing this script then

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,6 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
-// Import all of Bootstrap's JS
-//import * as bootstrap from 'bootstrap'
-
 import 'bootstrap'
 
 export * from './calculator'
